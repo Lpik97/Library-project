a772d8a8170611dd58002f145541446f0d4d8e73
Lpik97 <Lpik97@users.noreply.github.com>
2023-06-10 16:05:29 +0200

    fix(function AddBookToLibrary properties and name)
    The function name changed to render and its properties were modified.

--- script.js
+++ script.js
@@ -11,11 +11,27 @@ function addBookToLibrary(title, author, pages) {
   myLibrary.push(new Book(title, author, pages));
 }
 
-function displayBooks() {
-  const booksSection = document.body.querySelector('.books');
-  myLibrary.forEach(book => {
-    const card = document.createElement('div');
-    card.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}`;
-    booksSection.appendChild(card);
-  });
+function render() {
+  let library = document.querySelector('#library');
+  library.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+    <div class="card-header" href>
+      <div class="buttons">
+        <img src="${book.read ? './svg/visibility_off_FILL0_wght400_GRAD0_opsz48.svg' : './svg/visibility_FILL0_wght400_GRAD0_opsz48.svg'}" alt="read icon" class="status-btn" onclick="changeStatus(event)" data-index="${i}">
+        <img src="./svg/delete_FILL0_wght400_GRAD0_opsz48.svg" alt="delete icon" class="delete-btn" onclick="removeBook(${i})">
+      </div>
+      <h3 class="title">${book.title}</h3>
+      <h5 class="author">by: ${book.author}</h5>
+    </div>
+    <div class="card-body">
+      <p>Pages: ${book.pages}</p>
+      <p class="read-status">${book.read ? "Read" : "Not read yet"}</p>
+    </div>
+    `;
+    library.appendChild(card);
+  }
 }
