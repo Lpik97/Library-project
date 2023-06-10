6c9de7f5a02a8cc93fbd0a2e37e4fb27aa0469fb
Lpik97 <Lpik97@users.noreply.github.com>
2023-06-10 16:03:57 +0200

    fix(function Book properties)
    A new parameter was added to the function. A new value was added to the object.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary(title, author, pages) {
